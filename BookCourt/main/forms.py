61eed0a450a2a513e0754f3fe37e83b217f26874
Sami <oleg20011016@yandex.ru>
2023-02-24 18:42:52 +1000

    Добавил регистрацию (почти)

--- BookCourt/main/forms.py
+++ BookCourt/main/forms.py
@@ -9,37 +9,37 @@ class UsersForm(ModelForm):
 
         widgets = {
             "company_name": TextInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Название компании',
                 'required': 'required'
             }),
             "mobile_phone": TextInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Телефон',
                 'required': 'required'
             }),
             "email": EmailInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Email',
                 'required': 'required'
             }),
             "url": URLInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Ссылка на сайт',
                 'required': 'required'
             }),
             "adres": TextInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Адрес',
                 'required': 'required'
             }),
             "name_of_major": TextInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Представитель (ФИО)',
                 'required': 'required'
             }),
             "password": PasswordInput(attrs={
-                'class': 'form-field',
+                'class': 'input',
                 'placeholder': 'Пароль',
                 'required': 'required'
             })
